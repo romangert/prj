180db278a1de4d6e2d7362a74f9d4255eff869c8
Roman Gertopsky <roman.gert@gmail.com>
2020-01-29 19:11:48 +0200

    Add files via upload

---
+++ Prj/WF-Chain-of-Responsibility-POC/WF-Chain-of-Responsibility-POC/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // lets create employees 
+            TeamLeader tl = new TeamLeader();
+            ProjectLeader pl = new ProjectLeader();
+            HR hr = new HR();
+
+            // Now lets set the hierarchy of employees
+            tl.Supervisor = pl;
+            pl.Supervisor = hr;
+
+            // Now lets apply 5 day leave my TL
+            tl.ApplyLeave(new Leave(Guid.NewGuid(), 5));
+
+            // Now lets apply 15 day leave my TL
+            tl.ApplyLeave(new Leave(Guid.NewGuid(), 15));
+
+            // Now lets apply 25 day leave my TL
+            tl.ApplyLeave(new Leave(Guid.NewGuid(), 25));
+
+            // Now lets apply 35 day leave my TL
+            tl.ApplyLeave(new Leave(Guid.NewGuid(), 35));
+
+            Console.ReadLine();
+        }
+    }
+}
